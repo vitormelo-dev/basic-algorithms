30946ea87669cafa3a5ad774cde0f08cf674790f
Vitor <vitor.manoel@ajdesenvolvimento.com.br>
2024-12-24 06:55:16 -0300

    First Commit

---
+++ insertion-sort/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text.Json;
+
+int[] list = { 2, 3, 4, 1, 6 };
+
+for (int i = 1; i < list.Length; i++)
+{
+    int j = i - 1;
+
+    while(j >= 0 && list[j+1] < list[j])
+    {
+        int temp = list[j + 1];
+
+        list[j + 1] = list[j];
+        list[j] = temp;
+
+        j--;
+    }
+}
+
+Console.WriteLine(JsonSerializer.Serialize(list));
